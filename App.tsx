dc14df90a743e191d16ef5241c1973d5a87a8186
dooyeong20 <noviced22@gmail.com>
2021-11-11 18:29:42 +0900

    :balloon: 다크 테마 옵션 적용

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
-
+import { useColorScheme } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { ThemeProvider } from 'styled-components/native';
 import { Tabs } from './navigation/Tab';
+import { darkTheme, lightTheme } from './theme/styled';
 
 export default function App() {
+  const isDarkTheme = useColorScheme() === 'dark';
+
   return (
-    <>
-      <NavigationContainer theme={DefaultTheme}>
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+      <NavigationContainer>
         <Tabs />
       </NavigationContainer>
       <StatusBar style="auto" />
-    </>
+    </ThemeProvider>
   );
 }
---
+++ theme/styled.ts
@@ -0,0 +1,9 @@
+export const lightTheme = {
+  bgColor: '#fff',
+  textColor: '#1e272e',
+};
+
+export const darkTheme = {
+  bgColor: '#1e272e',
+  textColor: '#d2dae2',
+};
