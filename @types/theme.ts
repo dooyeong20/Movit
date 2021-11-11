3fff2bf2eb0929ed31fc94f229ba3034dbb2288d
dooyeong20 <noviced22@gmail.com>
2021-11-11 19:30:14 +0900

    :balloon: 타입 추가

---
+++ @types/theme.ts
@@ -0,0 +1,5 @@
+export type TDefaultTheme = {
+  bgColor: string;
+  textColor: string;
+  highlight: string;
+};
--- theme/styled.ts
+++ theme/styled.ts
@@ -1,9 +1,13 @@
-export const lightTheme = {
+import { TDefaultTheme } from '../@types/theme';
+
+export const lightTheme: TDefaultTheme = {
   bgColor: '#fff',
   textColor: '#1e272e',
+  highlight: '#7d5fff',
 };
 
-export const darkTheme = {
+export const darkTheme: TDefaultTheme = {
   bgColor: '#1e272e',
   textColor: '#d2dae2',
+  highlight: '#ffa801',
 };
