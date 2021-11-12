6b96dd0805bd417f27cbd5d5c73c38c9863650d9
dooyeong20 <noviced22@gmail.com>
2021-11-12 14:01:51 +0900

    :factory: Poster 컴포넌트 분리

---
+++ components/Poster.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import { makeImgPath } from '../utils';
+
+interface IProps {
+  path: string;
+}
+
+const PosterImage = styled.Image`
+  width: 100px;
+  height: 165px;
+  border-radius: 5px;
+`;
+
+export function Poster({ path }: IProps) {
+  return <PosterImage source={{ uri: makeImgPath(path) }} />;
+}
