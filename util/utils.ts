51a40f811c877433020d97cd999bc941edf14ab1
dooyeong20 <noviced22@gmail.com>
2021-11-12 15:00:53 +0900

    :factory: 디렉터리 이동

--- util/utils.ts
+++ util/utils.ts
@@ -2,4 +2,4 @@ const DEFAULT_IMG_PATH =
   'https://image.tmdb.org/t/p/w500/lNyLSOKMMeUPr1RsL4KcRuIXwHt.jpg';
 
 export const makeImgPath = (img: string, width = 'w500') =>
-  img ? `https://image.tmdb.org/t/p/${width}${img}` : DEFAULT_IMG_PATH;
+  img ? `https://image.tmdb.org/t/p/${width}/${img}` : DEFAULT_IMG_PATH;
