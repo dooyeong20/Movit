7d4a5add12416260e37fe39581a3a8a05cffcc87
dooyeong20 <noviced22@gmail.com>
2021-11-12 16:45:54 +0900

    :balloon: 테스트 적용

--- __tests__/utilTest.test.ts
+++ __tests__/utilTest.test.ts
@@ -1,8 +1,19 @@
-import { makeImgPath } from '../util/utils';
-test('Image path test', () => {
-  expect(makeImgPath('hello')).toBe('https://image.tmdb.org/t/p/w500/hello');
+import { makeImgPath, getFormatDate } from '../util/utils';
 
-  expect(makeImgPath(undefined as unknown as string)).toBe(
-    'https://image.tmdb.org/t/p/w500/lNyLSOKMMeUPr1RsL4KcRuIXwHt.jpg'
-  );
+describe('Test for util functions', () => {
+  test('Image path test', () => {
+    expect(makeImgPath('hello')).toBe('https://image.tmdb.org/t/p/w500/hello');
+
+    expect(makeImgPath(undefined as unknown as string)).toBe(
+      'https://image.tmdb.org/t/p/w500/lNyLSOKMMeUPr1RsL4KcRuIXwHt.jpg'
+    );
+  });
+
+  test('Format date test', () => {
+    expect(getFormatDate('2021-11-12')).toBe('2021년 11월 12일');
+
+    expect(getFormatDate('2021-01-02')).toBe('2021년 1월 2일');
+
+    expect(getFormatDate('2021-12-31')).toBe('2021년 12월 31일');
+  });
 });
