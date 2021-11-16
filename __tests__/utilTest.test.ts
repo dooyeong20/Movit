616a3de8a704e6741fc91d91c690b516e6980853
dooyeong20 <noviced22@gmail.com>
2021-11-16 14:10:24 +0900

    :balloon: 테스팅 문서 작성

--- __tests__/utilTest.test.ts
+++ __tests__/utilTest.test.ts
@@ -1,19 +1,25 @@
-import { makeImgPath, getFormatDate } from '../util/utils';
+import { makeImgPath, getFormatDate } from '../util';
 
 describe('Test for util functions', () => {
-  test('Image path test', () => {
+  test('Image path test (not undefined)', () => {
     expect(makeImgPath('hello')).toBe('https://image.tmdb.org/t/p/w500/hello');
+  });
 
+  test('Image path test (undefined)', () => {
     expect(makeImgPath(undefined as unknown as string)).toBe(
       'https://image.tmdb.org/t/p/w500/lNyLSOKMMeUPr1RsL4KcRuIXwHt.jpg'
     );
   });
 
-  test('Format date test', () => {
+  test('Format date test (2021-11-12) => 2021년 11월 12일 ', () => {
     expect(getFormatDate('2021-11-12')).toBe('2021년 11월 12일');
+  });
 
+  test('Format date test (2021-01-02) => 2021년 1월 2일 ', () => {
     expect(getFormatDate('2021-01-02')).toBe('2021년 1월 2일');
+  });
 
+  test('Format date test (2021-12-31) => 2021년 12월 31일 ', () => {
     expect(getFormatDate('2021-12-31')).toBe('2021년 12월 31일');
   });
 });
--- docs/testing.md
+++ docs/testing.md
@@ -1,3 +1,7 @@
 # Testing
 
+## Test Result
+
+![test image](./test.png)
+
 작성 중입니다...
