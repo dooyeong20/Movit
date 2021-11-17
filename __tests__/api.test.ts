7479e664fbc458eeaba50e65e3fad9b57021cdaa
dooyeong20 <noviced22@gmail.com>
2021-11-17 23:52:54 +0900

    :balloon: API 테스트코드 작성

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -1,3 +1,64 @@
-describe('Test for apis', () => {
-  test('Image path test (not undefined)', () => {});
+import fetchMock from 'jest-fetch-mock';
+import { movieAPI, tvAPI } from '../Api';
+
+describe('Test for APIs', () => {
+  beforeEach(() => {
+    fetchMock.doMock();
+  });
+
+  test('Movie Trending API Test', async () => {
+    const data = await movieAPI.trending();
+
+    expect(data).toHaveProperty('results');
+    expect(data.results).not.toHaveLength(0);
+  });
+
+  test('Movie Now Playing API Test', async () => {
+    const data = await movieAPI.nowPlaying();
+
+    expect(data).toHaveProperty('results');
+    expect(data.results).not.toHaveLength(0);
+  });
+
+  test('Movie Up Coming API Test', async () => {
+    const data = await movieAPI.upcoming();
+
+    expect(data).toHaveProperty('results');
+    expect(data.results).not.toHaveLength(0);
+  });
+
+  test('TV Airing Today API Test', async () => {
+    const data = await tvAPI.airingToday();
+
+    expect(data).toHaveProperty('results');
+    expect(data.results).not.toHaveLength(0);
+  });
+
+  test('TV On Air API Test', async () => {
+    const data = await tvAPI.onTheAir();
+
+    expect(data).toHaveProperty('results');
+    expect(data.results).not.toHaveLength(0);
+  });
+
+  test('TV Popular API Test', async () => {
+    const data = await tvAPI.popular();
+
+    expect(data).toHaveProperty('results');
+    expect(data.results).not.toHaveLength(0);
+  });
+
+  test('TV Top Rated API Test', async () => {
+    const data = await tvAPI.topRated();
+
+    expect(data).toHaveProperty('results');
+    expect(data.results).not.toHaveLength(0);
+  });
+
+  test('TV Trending API Test', async () => {
+    const data = await tvAPI.trending();
+
+    expect(data).toHaveProperty('results');
+    expect(data.results).not.toHaveLength(0);
+  });
 });
