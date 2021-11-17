f1d48b82e59e57eac28604ce7fa1ae62868621a8
dooyeong20 <noviced22@gmail.com>
2021-11-17 23:07:35 +0900

    :balloon: 한국 api 로 변경

--- Api/tv.ts
+++ Api/tv.ts
@@ -2,22 +2,22 @@ import { API_KEY, BASE_URL } from '../const';
 
 const topRated = () =>
   fetch(
-    `${BASE_URL}/tv/top_rated?api_key=${API_KEY}&language=en-US&page=1`
+    `${BASE_URL}/tv/top_rated?api_key=${API_KEY}&language=ko-KR&page=1`
   ).then((res) => res.json());
 
 const popular = () =>
-  fetch(`${BASE_URL}/tv/popular?api_key=${API_KEY}&language=en-US&page=1`).then(
-    (res) => res.json()
-  );
+  fetch(
+    `${BASE_URL}/tv/popular?api_key=${API_KEY}&language=ko-KR&page=1&region=KR`
+  ).then((res) => res.json());
 
 const onTheAir = () =>
   fetch(
-    `${BASE_URL}/tv/on_the_air?api_key=${API_KEY}&language=en-US&page=1`
+    `${BASE_URL}/tv/on_the_air?api_key=${API_KEY}&language=ko-KR&page=1&region=KR`
   ).then((res) => res.json());
 
 const airingToday = () =>
   fetch(
-    `${BASE_URL}/tv/airing_today?api_key=${API_KEY}&language=en-US&page=1`
+    `${BASE_URL}/tv/airing_today?api_key=${API_KEY}&language=ko-KR&page=1&region=KR`
   ).then((res) => res.json());
 
 const trending = () =>
