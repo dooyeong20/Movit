23d81c7e9405dff5b831f076023cd165a2537ede
dooyeong20 <noviced22@gmail.com>
2021-11-18 13:46:15 +0900

    :balloon: 검색 화면 구현

--- Api/serach.ts
+++ Api/serach.ts
@@ -1,13 +1,21 @@
+import { QueryFunctionContext } from 'react-query';
 import { API_KEY, BASE_URL } from '../const';
 
-const movies = (query: string) => () =>
-  fetch(
+const movies = ({ queryKey }: QueryFunctionContext) => {
+  const [_, query] = queryKey;
+
+  console.log(query);
+  return fetch(
     `${BASE_URL}/search/movie?api_key=${API_KEY}&language=ko-KR&page=1&query=${query}`
   ).then((res) => res.json());
+};
 
-const tvShows = (query: string) => () =>
-  fetch(
+const tvShows = ({ queryKey }: QueryFunctionContext) => {
+  const [_, query] = queryKey;
+
+  return fetch(
     `${BASE_URL}/search/tv?api_key=${API_KEY}&language=ko-KR&page=1&query=${query}`
   ).then((res) => res.json());
+};
 
 export const searchAPI = { movies, tvShows };
--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,10 +1,69 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import styled, { useTheme } from 'styled-components/native';
+import _ from 'lodash';
+import { useQuery } from 'react-query';
+import { searchAPI } from '../Api';
+import { HList, Loader } from '../components';
+import { BaseResponse } from '../@types';
+
+const Container = styled.ScrollView``;
+
+const SearchBar = styled.TextInput`
+  border: 2px solid ${(props) => props.theme.textColor};
+  padding: 5px 15px;
+  border-radius: 500px;
+  width: 90%;
+  margin: 20px auto;
+  color: ${(props) => props.theme.textColor};
+`;
 
 export default function Search() {
+  const { textColor } = useTheme();
+  const [query, setQuery] = useState('');
+  const {
+    isLoading: movieLoading,
+    data: movieData,
+    refetch: searchMovies,
+  } = useQuery<BaseResponse>(['searchMovies', query], searchAPI.movies, {
+    enabled: false,
+  });
+  const {
+    isLoading: tvLoading,
+    data: tvData,
+    refetch: searchTvs,
+  } = useQuery<BaseResponse>(['searchTvs', query], searchAPI.tvShows, {
+    enabled: false,
+  });
+
+  useEffect(() => {
+    searchMovies();
+    searchTvs();
+  }, [query, searchMovies, searchTvs]);
+
+  const handleChangeText = (text: string) => {
+    setQuery(text);
+  };
+
+  const isLoading = movieLoading || tvLoading;
+
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
+    <Container
+      contentContainerStyle={{
+        flex: 1,
+      }}
+    >
+      <SearchBar
+        placeholder="Search Movies or TV Shows"
+        placeholderTextColor={textColor}
+        returnKeyLabel="search"
+        returnKeyType="search"
+        onChangeText={_.debounce(handleChangeText, 500)}
+      />
+      {isLoading && <Loader />}
+      {movieData?.total_results ? (
+        <HList title="Movie" data={movieData} />
+      ) : null}
+      {tvData?.total_results ? <HList title="TV Shows" data={tvData} /> : null}
+    </Container>
   );
 }
