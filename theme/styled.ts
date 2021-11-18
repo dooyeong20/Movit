bb3273e00c2475c237b1a92a919231701ab6bd9a
dooyeong20 <noviced22@gmail.com>
2021-11-18 13:46:00 +0900

    :pen: 테마 색상 수정

--- theme/styled.ts
+++ theme/styled.ts
@@ -1,7 +1,7 @@
 import { TDefaultTheme } from '../@types/theme';
 
 export const lightTheme: TDefaultTheme = {
-  bgColor: '#fff',
+  bgColor: '#f1f2f6',
   textColor: '#1e272e',
   highlight: '#7d5fff',
 };
