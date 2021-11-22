eafb874e44cd0ab1dedb506676e5d3996cd9c875
dooyeong20 <noviced22@gmail.com>
2021-11-22 16:55:49 +0900

    :balloon: Detail 스크린 VieoLink & Genre 구현

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -1,10 +1,11 @@
 import { LinearGradient } from 'expo-linear-gradient';
 import React, { useEffect } from 'react';
-import { Dimensions, StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet, Linking } from 'react-native';
+import { useQuery } from 'react-query';
 import styled from 'styled-components/native';
-
 import { DetailProps } from '../@types';
-import { Poster } from '../components';
+import { movieAPI, tvAPI } from '../Api';
+import { Genre, Loader, Poster, VideoLink } from '../components';
 import { makeImgPath } from '../util';
 
 const { height: SCREEN_HEIGHT } = Dimensions.get('window');
@@ -32,14 +33,27 @@ const Title = styled.Text`
 const Overview = styled.Text`
   color: ${(props) => props.theme.textColor};
   font-size: 15px;
-  margin-top: 10px;
+  margin: 20px 0;
+`;
+
+const Data = styled.View`
   padding: 0 20px;
+`;
+
+const GenreContainer = styled.View`
+  flex-direction: row;
+  margin-top: 5px;
 `;
 
 export function Detail({
   route: { params },
   navigation: { setOptions },
 }: DetailProps) {
+  const isMovie = 'original_title' in params;
+  const { isLoading, data } = useQuery(
+    [isMovie ? 'movie' : 'tv', params.id],
+    isMovie ? movieAPI.detail : tvAPI.detail
+  );
   const detailTitle =
     params.title ||
     params.original_title ||
@@ -52,6 +66,12 @@ export function Detail({
         'original_title' in params || 'title' in params ? 'Movie' : 'TV Show',
     });
   }, [detailTitle, params, setOptions]);
+
+  const handleClickLink = (videoId: string) => async () => {
+    const url = `https://m.youtube.com/watch?v=${videoId}`;
+    await Linking.openURL(url);
+  };
+
   return (
     <Container>
       <Header>
@@ -69,7 +89,23 @@ export function Detail({
           <Title>{detailTitle}</Title>
         </Column>
       </Header>
-      <Overview>{params.overview}</Overview>
+      <Data>
+        <GenreContainer>
+          {data?.genres?.map(({ id, name }: { name: string; id: number }) => (
+            <Genre key={id} name={name} />
+          ))}
+        </GenreContainer>
+        <Overview>{params.overview}</Overview>
+        {isLoading ? <Loader /> : null}
+        {data?.videos?.results?.map((video: any) => (
+          <VideoLink
+            key={video.id}
+            onPressLink={handleClickLink}
+            title={video.name}
+            videoId={video.key}
+          />
+        ))}
+      </Data>
     </Container>
   );
 }
