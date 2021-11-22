47c6b4d60cfc53f1b8712b08af83936a9ea7d7a6
dooyeong20 <noviced22@gmail.com>
2021-11-22 23:10:28 +0900

    :balloon: 영화관 정보 추가

--- const.ts
+++ const.ts
@@ -1,2 +1,16 @@
 export const API_KEY = '59ee2230f87d37d483a3a52eb8235751';
 export const BASE_URL = 'https://api.themoviedb.org/3';
+export const THEATER_LIST = [
+  {
+    name: 'CGV',
+    url: 'https://m.cgv.co.kr/WebApp/Reservation/QuickResult.aspx',
+  },
+  {
+    name: 'Mega Box',
+    url: 'https://m.megabox.co.kr/booking',
+  },
+  {
+    name: 'Lotte Cinema',
+    url: 'https://m.lottecinema.co.kr/NLCHS/Ticketing',
+  },
+];
