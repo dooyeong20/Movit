52b78fc2893c28010df0eaac5c0a5ae7fc1ff0b5
dooyeong20 <noviced22@gmail.com>
2021-11-22 16:54:56 +0900

    :pen: type 수정

--- @types/api.ts
+++ @types/api.ts
@@ -1,3 +1,3 @@
 import { QueryFunctionContext } from 'react-query';
 
-export type Tfetcher = (context: QueryFunctionContext) => Promise<Response>;
+export type Tfetcher = (context: QueryFunctionContext) => Promise<any>;
