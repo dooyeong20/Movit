46ae28cf4cf2b4b9148120efb221bb8c485e4cf6
dooyeong20 <noviced22@gmail.com>
2021-11-27 16:04:18 +0900

    :balloon: Tabs 상위에 Like Provider 적용

--- navigation/Tabs.tsx
+++ navigation/Tabs.tsx
@@ -5,6 +5,7 @@ import { Tv } from '../screens/Tv';
 import { Ionicons } from '@expo/vector-icons';
 import { useTheme } from 'styled-components/native';
 import Search from '../screens/Search';
+import { LikeProvider } from '../provider/LikeProvider';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,50 +13,52 @@ export function Tabs() {
   const { bgColor, textColor, highlight } = useTheme();
 
   return (
-    <Tab.Navigator
-      screenOptions={{
-        headerTitleAlign: 'center',
-        headerStyle: {
-          backgroundColor: bgColor,
-        },
-        headerTintColor: textColor,
-        tabBarInactiveTintColor: textColor,
-        tabBarActiveTintColor: highlight,
-        tabBarLabelStyle: {
-          marginBottom: 2,
-        },
-        tabBarStyle: {
-          backgroundColor: bgColor,
-        },
-      }}
-    >
-      <Tab.Screen
-        name="Movies"
-        component={Movies}
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="film-outline" color={color} size={size} />
-          ),
+    <LikeProvider>
+      <Tab.Navigator
+        screenOptions={{
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: bgColor,
+          },
+          headerTintColor: textColor,
+          tabBarInactiveTintColor: textColor,
+          tabBarActiveTintColor: highlight,
+          tabBarLabelStyle: {
+            marginBottom: 2,
+          },
+          tabBarStyle: {
+            backgroundColor: bgColor,
+          },
         }}
-      />
-      <Tab.Screen
-        name="TV"
-        component={Tv}
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="tv-outline" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Search"
-        component={Search}
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="search-outline" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+      >
+        <Tab.Screen
+          name="Movies"
+          component={Movies}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="film-outline" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="TV"
+          component={Tv}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="tv-outline" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Search"
+          component={Search}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="search-outline" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </LikeProvider>
   );
 }
